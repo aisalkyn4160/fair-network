10deb7c1ead3328c5427081c7bd02ce55938d2f5
aisalkyn4160 <aisalkyn.4160@gmail.com>
2024-08-22 11:15:45 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const header = document.querySelector(".header");
+const burger = document.querySelector(".burger");
+
+
+burger.addEventListener("click", () => {
+    header.classList.toggle("mobile-header");
+    if (header.classList.contains("mobile-header")) {
+        document.body.classList.add("no-scroll");
+    } else {
+        document.body.classList.remove("no-scroll");
+    }
+})
+
+// -------------------------------------popup--------------------------------------
+const popup = document.querySelector('.popup')
+const popupShowBtns = document.querySelectorAll('.show-popup')
+const closeFormBtn = document.querySelector('.close-form')
+
+popupShowBtns.forEach(item => {
+    item.addEventListener('click', () => {
+        popup.classList.add('show-popup')
+        document.body.classList.add('no-scroll')
+    })
+})
+
+closeFormBtn.addEventListener('click', () => {
+    popup.classList.remove('show-popup')
+    document.body.classList.remove('no-scroll')
+})
+
+document.addEventListener('click', (event) => {
+    if (event.target === popup) {
+        popup.classList.remove('show-popup')
+        document.body.classList.remove('no-scroll')
+    }
+});
